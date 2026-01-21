10d99868479ab9c8a8c19f958957866f9e268a38
Adithi Suresh <143402066+adithisuresh2205@users.noreply.github.com>
2026-01-21 20:18:00 +0530

    Create ProgressUpdateDTO.java

---
+++ LifestyleSurveyBackend/LifestyleSurveyBackend/dto/ProgressUpdateDTO.java
@@ -0,0 +1,15 @@
+package com.lifestyle.survey.dto;
+
+public class ProgressUpdateDTO {
+
+    private Long goalId;
+    private Double currentValue;
+
+    public Long getGoalId() {
+        return goalId;
+    }
+
+    public Double getCurrentValue() {
+        return currentValue;
+    }
+}
